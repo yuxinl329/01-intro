7d416e044a2393350cf42684001b4da9aa652234
Yuxin <yuxinl329@gmail.com>
2020-09-14 13:34:01 -0400

    changes

--- DList.hs
+++ DList.hs
@@ -1,22 +1,55 @@
 module DList where
+
 import Test.HUnit
 
 type DList a = [a] -> [a]
 
 list :: [Int]
-list = 1 : 2 : 3 : []  -- end is nil
+list = 1 : 2 : 3 : [] -- end is nil
 
 dlist :: DList Int
-dlist =  \x -> 1 : 2 : 3 : x -- end is "x"
+dlist = \x -> 1 : 2 : 3 : x -- end is "x"
 
 empty :: DList a
-empty = undefined
+empty = returnEmpty
+  where
+    returnEmpty :: [a] -> [a]
+    returnEmpty ys = ys
 
 singleton :: a -> DList a
-singleton x = undefined
+singleton x = returnSingleton x
+  where
+    returnSingleton :: a -> [a] -> [a]
+    returnSingleton x ys = x : ys
+
+testSingleton :: Test
+testSingleton =
+  "singleton"
+    ~: TestList
+      [ singleton 'a' [] ~?= ['a'],
+        singleton 'a' ['b', 'c'] ~?= ['a', 'b', 'c']
+      ]
+
+runTestSingleton :: IO Counts
+runTestSingleton = runTestTT testSingleton
 
 append :: DList a -> DList a -> DList a
-append = undefined
+append xs ys = appendAux (toList xs) (toList ys)
+  where
+    appendAux :: [a] -> [a] -> DList a
+    appendAux [] ys = returnDList ys
+      where
+        returnDList :: [a] -> [a] -> [a]
+        returnDList ys _ = ys
+    appendAux (x : xs) ys = appendAux xs (x : ys)
+
+testAppend :: Test
+testAppend =
+  "append"
+    ~: [toList (append (singleton 1) (singleton 2)) ~?= [1, 2]]
+
+runTestAppend :: IO Counts
+runTestAppend = runTestTT testAppend
 
 cons :: a -> DList a -> DList a
 cons = undefined
@@ -29,26 +62,30 @@ toList x = x []
 
 testDList :: IO ()
 testDList = do
-  _ <- runTestTT $ TestList [
-          toList empty ~?= ([] :: [Char])
-
-       ]
+  _ <-
+    runTestTT $
+      TestList
+        [ toList empty ~?= ([] :: [Char])
+        ]
   return ()
 
 micro1 :: Char
-micro1 = last (t 10000 "") where
-  t 0 l = l
-  t n l = t (n-1) (l ++ "s")
+micro1 = last (t 10000 "")
+  where
+    t 0 l = l
+    t n l = t (n -1) (l ++ "s")
 
 micro2 :: Char
-micro2 = last (toList (t 10000 empty)) where
-   t 0 l = l
-   t n l = t (n-1) (l `append` singleton 's')
+micro2 = last (toList (t 10000 empty))
+  where
+    t 0 l = l
+    t n l = t (n -1) (l `append` singleton 's')
 
 naiveReverse :: [a] -> [a]
-naiveReverse = rev where
-  rev []       = []
-  rev (x : xs) = rev xs ++ [x]
+naiveReverse = rev
+  where
+    rev [] = []
+    rev (x : xs) = rev xs ++ [x]
 
 bigList :: [Int]
 bigList = [0 .. 10000]
@@ -57,15 +94,16 @@ micro3 :: Int
 micro3 = last (naiveReverse bigList)
 
 ivoryTowerReverse :: [a] -> [a]
-ivoryTowerReverse = foldr (flip (++) . (:[]))  []
+ivoryTowerReverse = foldr (flip (++) . (: [])) []
 
 micro4 :: Int
 micro4 = last (ivoryTowerReverse bigList)
 
 dlistReverse :: [a] -> [a]
-dlistReverse = toList . rev where
-  rev []       = empty
-  rev (x : xs) = rev xs `append` singleton x
+dlistReverse = toList . rev
+  where
+    rev [] = empty
+    rev (x : xs) = rev xs `append` singleton x
 
 micro5 :: Int
 micro5 = last (dlistReverse bigList)
@@ -78,4 +116,3 @@ micro6 = last (dlistIvoryTowerReverse bigList)
 
 micro7 :: Int
 micro7 = last (reverse bigList)
-
--- SecretCode.hs
+++ SecretCode.hs
@@ -4,37 +4,44 @@ import Data.Char
 import Data.Maybe
 import Test.HUnit
 
-code :: [(Char,Char)]
+code :: [(Char, Char)]
 code = zip ['a' .. 'z'] cypher ++ zip ['A' .. 'Z'] (map toUpper cypher)
   where
     cypher :: String
     cypher = "thequickbrownfxjmpsvlazydg"
 
 encodeChar :: Char -> Char
-encodeChar c = undefined
+encodeChar c = fromMaybe c (lookup c code)
 
-testEncodeChar = runTestTT $ TestList [ encodeChar 'a' ~?= 't',
-                                        encodeChar '.' ~?= '.']
+testEncodeChar :: IO Counts
+testEncodeChar =
+  runTestTT $
+    TestList
+      [ encodeChar 'a' ~?= 't',
+        encodeChar '.' ~?= '.'
+      ]
 
 encodeLine :: String -> String
-encodeLine = undefined
+encodeLine = map encodeChar
 
 testEncodeLine = runTestTT $ TestList [encodeLine "abc defgh" ~?= "the quick"]
 
 encodeContent :: String -> String
-encodeContent = undefined
+encodeContent str = (unlines . reverse . map encodeLine . lines) str
 
-testEncodeContent = runTestTT $
-  encodeContent "abc\n defgh\n" ~?= " quick\nthe\n"
+testEncodeContent =
+  runTestTT $
+    encodeContent "abc\n defgh\n" ~?= " quick\nthe\n"
 
 encodeFile :: FilePath -> IO ()
 encodeFile f = do
   let outFile = f ++ ".code"
-  undefined
+  contents <- readFile f
+  writeFile outFile (encodeContent f)
 
 main :: IO ()
-main = do putStrLn "What file shall I encode?"
-          fn <- getLine
-          encodeFile fn
-          putStrLn "All done!"
-
+main = do
+  putStrLn "What file shall I encode?"
+  fn <- getLine
+  encodeFile fn
+  putStrLn "All done!"
